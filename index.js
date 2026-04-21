c8bb1d31ddf7531ad5ee0573e856aeaadcfe9e29
Jasmine Malungila <malungilajasmine@gmail.com>
2026-04-21 10:05:04 -0400

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1;
+code1 = 7 + 3;
+console.log(code1);
+let code2 = 60 - 40 / 2;
+console.log(code2);
+let code3 = 37 + (5 % 3);
+console.log(code3);
+
+const message = "The vault has been secured. The combonation is:";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
